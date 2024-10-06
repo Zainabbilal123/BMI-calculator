80c1ad9d661ac0b8ba94ad009cd05206375fee70
Your Name <bilalzainab123agmail.com>
2024-10-06 16:45:47 +0500

    added javascript

--- javascript.js
+++ javascript.js
@@ -10,9 +10,23 @@ buttonresult.addEventListener('click' , buttonresult);
 function buttonresult {
     weight.parsefloat('weight');
     height.parsefloat('height');
-}
+
 
 var formula = height*2/weight;
 var calculated = formula.tofixed[2]
 
-var 
+results.textContent= 'result: ${result}';
+
+var category
+if (calculated < 18.5) {
+     category = 'underwight'
+} else if (calculated < 24.9) {
+    category =" Normal weight"
+} else if(calculated < 29.9) {
+    category = "overweight"
+} else (calculated > 30) {
+    category = "obesity"
+}
+
+category.textContent = 'category: ${category}';
+}
