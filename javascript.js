01d34835cff2f7f58ce1bd626bbfebafe65fb75c
Your Name <bilalzainab123agmail.com>
2024-10-06 16:31:38 +0500

    did little bit javacsript

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-var weight = document.
+var weight = document.getElementById('weight');
+var height =document.getElementById('height');
+var buttonresult = document.getElementById('buttonresult');
+var results = document.getElementById('results');
+var category = document.getElementById('category');
+
+
+buttonresult.addEventListener('click' , buttonresult);
+  
+function buttonresult {
+    weight.parsefloat('weight');
+    height.parsefloat('height');
+}
+
+var formula = height*2/weight;
+var calculated = formula.tofixed[2]
+
+var 
