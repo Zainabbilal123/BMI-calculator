c5f1ab1a53ad06557269adabaa83fb342a55c6e7
Your Name <bilalzainab123agmail.com>
2024-10-08 18:14:45 +0500

    done with javacsript

--- javascript.js
+++ javascript.js
@@ -6,30 +6,36 @@ const categoryList = document.getElementById('category');
 
 
 resultValue.addEventListener('click', calculatedBMI);
+
+
   
 function calculatedBMI() {
    const weightValue = parseFloat(weight.value);
     const heightValue = parseFloat(height.value);
 
    
-
 const formula = weightValue / ( heightValue ** 2);
 const solvedNumber = formula.toFixed(2);
 
      bmiResult.textContent= `BMI: ${solvedNumber};`
-
+     
 let categoryMsg;
-if (formula < 18.5) {
+if (formula <= 18.5) {
      categoryMsg = 'underweight';
-} else if (formula < 24.9) {
+} else if (formula <= 24.9) {
     categoryMsg =" Normal weight";
-} else if(formula < 29.9) {
+} else if(formula <= 29.9) {
     categoryMsg = "overweight";
 } else  {
     categoryMsg = "obesity";
 }
 
   categoryList.textContent = categoryMsg;
+
+  weight.value = ""
+  height.value = ""
+
+  
 }
 
 
